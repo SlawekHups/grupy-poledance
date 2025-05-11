3d312bfb9fad1f51723c6624a829b759d9c53a00
Slawek Hupka <121352010+SlawekHups@users.noreply.github.com>
2025-05-11 17:22:15 +0200

    Usunieto wyswietlanie ID

--- app/Filament/Admin/Resources/GroupResource.php
+++ app/Filament/Admin/Resources/GroupResource.php
@@ -35,6 +35,7 @@ class GroupResource extends Resource
     {
         return $table
             ->columns([
+                TextColumn::make('id')->searchable(),
                 TextColumn::make('name')->searchable(),
             ])
             ->filters([
