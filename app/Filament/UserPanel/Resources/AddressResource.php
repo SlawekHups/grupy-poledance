2abf2ea4da3690ad6588a77825d59e798b036691
Slawek Hupka <121352010+SlawekHups@users.noreply.github.com>
2025-05-15 21:31:41 +0200

    dodanie adresu dla usera do jego panelu

---
+++ app/Filament/UserPanel/Resources/AddressResource.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Filament\UserPanel\Resources;
+
+use App\Filament\UserPanel\Resources\AddressResource\Pages;
+use App\Filament\UserPanel\Resources\AddressResource\RelationManagers;
+use App\Models\Address;
+use Filament\Forms;
+use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class AddressResource extends Resource
+{
+    protected static ?string $model = Address::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+         return $form
+        ->schema([
+            Hidden::make('user_id')->default(fn () => auth()->id()),
+
+            TextInput::make(name: 'type')->required(),
+            TextInput::make('street'),
+            TextInput::make('postal_code'),
+            TextInput::make('city'),
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make(name: 'type')->label('Nazwa'),
+                TextColumn::make('street')->label('Ulica'),
+                TextColumn::make('postal_code')->label('Kod'),
+                TextColumn::make('city')->label('Miasto'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()->where('user_id', auth()->id());
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAddresses::route('/'),
+            'create' => Pages\CreateAddress::route('/create'),
+            'edit' => Pages\EditAddress::route('/{record}/edit'),
+        ];
+    }
+}
