55eb1c5375c4874ba55990c0d360d11022130151
Slawek Hupka <121352010+SlawekHups@users.noreply.github.com>
2025-07-16 14:44:19 +0200

    dodano ilosc obecnosci w danym miesiacu

--- app/Filament/UserPanel/Widgets/PaymentsStatsWidget.php
+++ app/Filament/UserPanel/Widgets/PaymentsStatsWidget.php
@@ -6,6 +6,7 @@ use Filament\Widgets\StatsOverviewWidget;
 use Filament\Widgets\StatsOverviewWidget\Card;
 use App\Models\Payment;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Attendance;
 
 class PaymentsStatsWidget extends StatsOverviewWidget
 {
@@ -14,6 +15,11 @@ class PaymentsStatsWidget extends StatsOverviewWidget
         $userId = Auth::id();
         $paymentsCount = Payment::where('user_id', $userId)->count();
         $paymentsSum = Payment::where('user_id', $userId)->where('paid', true)->sum('amount');
+        $attendancesThisMonth = Attendance::where('user_id', $userId)
+            ->where('present', true)
+            ->whereMonth('date', now()->month)
+            ->whereYear('date', now()->year)
+            ->count();
 
         return [
             Card::make('Liczba płatności', $paymentsCount)
@@ -23,6 +29,10 @@ class PaymentsStatsWidget extends StatsOverviewWidget
             Card::make('Suma płatności', number_format($paymentsSum, 2, ',', ' ') . ' zł')
                 ->icon('heroicon-o-currency-euro')
                 ->color('success'),
+            Card::make('Obecności w tym miesiącu', $attendancesThisMonth)
+                ->icon('heroicon-o-calendar-days')
+                ->color('info')
+                ->url(route('filament.user.resources.attendances.index')),
         ];
     }
 } 
