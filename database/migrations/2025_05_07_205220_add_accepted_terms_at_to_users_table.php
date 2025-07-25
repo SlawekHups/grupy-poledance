8db9f2429b2bf10a950cf8bb9d9d30c01a7a796b
Slawek Hupka <121352010+SlawekHups@users.noreply.github.com>
2025-07-25 14:28:56 +0200

    poprawka przy wyswietlaniu zaplecz oraz korekta przy wysylaniu do bazy

--- database/migrations/2025_05_07_205220_add_accepted_terms_at_to_users_table.php
+++ database/migrations/2025_05_07_205220_add_accepted_terms_at_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-           // $table->timestamp('accepted_terms_at')->nullable();
+            $table->timestamp('accepted_terms_at')->nullable();
         });
     }
 
