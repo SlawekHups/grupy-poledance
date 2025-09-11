1d5a1e40b9b2b8f7f1c3d7bc1b617b96d8cc770e
Slawek Hupka <121352010+SlawekHups@users.noreply.github.com>
2025-09-11 09:39:36 +0200

    feat: Dodaj linki do statystyk pre-rejestracji
    - Dodano URL do wszystkich statystyk pre-rejestracji
    - Umożliwiono filtrowanie statystyk według aktywności tokenów
    - Umożliwiono filtrowanie statystyk według użycia formularzy
    - Umożliwiono filtrowanie statystyk według wygasłych tokenów

--- app/Filament/Admin/Resources/PreRegistrationResource/Widgets/PreRegistrationStatsWidget.php
+++ app/Filament/Admin/Resources/PreRegistrationResource/Widgets/PreRegistrationStatsWidget.php
@@ -19,22 +19,26 @@ class PreRegistrationStatsWidget extends BaseWidget
             Stat::make('Wszystkie pre-rejestracje', $total)
                 ->description('Łączna liczba')
                 ->descriptionIcon('heroicon-m-users')
-                ->color('primary'),
+                ->color('primary')
+                ->url(route('filament.admin.resources.pre-registrations.index')),
                 
             Stat::make('Ważne tokeny', $valid)
                 ->description('Dostępne do użycia')
                 ->descriptionIcon('heroicon-m-clock')
-                ->color('success'),
+                ->color('success')
+                ->url(route('filament.admin.resources.pre-registrations.index', ['tableFilters[valid][isActive]' => '1'])),
                 
             Stat::make('Użyte', $used)
                 ->description('Wypełnione formularze')
                 ->descriptionIcon('heroicon-m-check-circle')
-                ->color('info'),
+                ->color('info')
+                ->url(route('filament.admin.resources.pre-registrations.index', ['tableFilters[used][value]' => 'true'])),
                 
             Stat::make('Wygasłe', $expired)
                 ->description('Nieaktywne tokeny')
                 ->descriptionIcon('heroicon-m-x-circle')
-                ->color('danger'),
+                ->color('danger')
+                ->url(route('filament.admin.resources.pre-registrations.index', ['tableFilters[expired][isActive]' => '1'])),
         ];
     }
 }
